9c34682b3d8cd4b3accf76aa3956a378745b9600
Guilherme Adriel <gui.adriel@gmail.com>
2021-06-05 23:19:52 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# guiadriel.github.io
---
+++ _next/static/4kXIuU6oFDR84ijMuhod-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-25bf462b6c5f3f3c5eb3.js"],"/_error":["static/chunks/pages/_error-b30902e13465df7c5366.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
